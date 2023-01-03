74bdd9d1c00cafcfa16da0231c6b1b9b23d3045e
Dang <d.long271293@gmail.com>
2023-01-03 17:55:17 +0700

    init file

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "ui";
+import { Button } from "react-daterangepicker-tz";
 
 export default function Docs() {
   return (
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { DateRangePicker } from "react-daterangepicker-tz";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button />
+      <DateRangePicker />
     </div>
   );
 }
