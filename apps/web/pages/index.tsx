36a54e3cd0872bdfbba09e6bf0c9751ad965a9f0
Long Dang <ldv@localizedirect.com>
2023-01-05 17:44:48 +0700

    update

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,17 @@
-import { DateRangePicker } from "react-daterangepicker-tz";
+import { Calendar, DateRangePicker } from "react-daterangepicker-tz";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <DateRangePicker />
+      <Calendar />
+      <div style={{ width: 600 }}>
+        <DateRangePicker
+          startDate={new Date("Wed Mar 01 2023 00:00:00 GMT+0700")}
+          endDate={new Date("Sat Mar 11 2023 00:00:00 GMT+0700")}
+          onChange={() => {}}
+        />
+      </div>
     </div>
   );
 }
